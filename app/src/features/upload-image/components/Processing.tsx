8df07e2b4881a4887b6b2062c058c7aa332b1c3d
Julian <foo@bar.com>
2022-07-17 12:24:49 +0200

    add processing anim with lottie

--- app/src/features/upload-image/components/Processing.tsx
+++ app/src/features/upload-image/components/Processing.tsx
@@ -1,17 +1,32 @@
-import { Spinner, Text, VStack } from 'native-base';
+import LottieView from 'lottie-react-native';
+import { Box, Text, VStack } from 'native-base';
+// https://lottiefiles.com/108979-image-scanning-finding-searching
+import animation from '../proccesing-animation.json';
 
 export const Processing = () => {
   return (
     <VStack alignItems="center">
-      <Spinner size="lg" />
+      <Box w="200" h="200">
+        <LottieView
+          source={animation}
+          autoPlay={true}
+          loop={true}
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+        />
+      </Box>
       <Text
         fontSize="lg"
         textTransform="uppercase"
         letterSpacing="lg"
         fontWeight="light"
-        mt={4}
       >
-        Processing your picture
+        Processing your image
+      </Text>
+      <Text fontSize="xs" letterSpacing="sm" fontWeight="light">
+        This may take a while depending on the size.
       </Text>
     </VStack>
   );
